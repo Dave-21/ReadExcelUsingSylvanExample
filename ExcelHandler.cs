2792f8130791aef75e6edc53d796bab7e0b5672a
Dave <davidrivard4@gmail.com>
2022-09-01 14:12:18 -0400

    first commit

---
+++ ExcelHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sylvan.Data.Excel;
+
+namespace ReadExcelUsingSylvan
+{
+    internal static class ExcelHandler
+    {
+        public static DataTable FixHeaders(DataTable dt)
+        {
+            foreach (DataColumn column in dt.Columns)
+            {
+                //these are a few (of many) illegal characters that aren't accepted in the System.Windows.Controls.DataGrid Column Header 
+                column.ColumnName = column.ColumnName.Replace(".", "_").Replace("#", "").Replace("/", " ").Replace(":", " ");
+            }
+
+            return dt;
+        }
+
+        public static DataTable SylvanReadExcelFile(string filePath)
+        {
+            var theData = new DataTable();
+
+            using var edr = ExcelDataReader.Create(filePath, new ExcelDataReaderOptions
+            {
+                GetErrorAsNull = true,
+            });
+
+            theData.Load(edr, LoadOption.PreserveChanges);
+
+            return theData;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static ReadExcelUsingSylvan.ExcelHandler;
+
+namespace ReadExcelUsingSylvan
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SelectFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+
+            fileDialog.ShowDialog();
+
+            System.Diagnostics.Stopwatch watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+
+            //issue is not with Sylvan.Data.Excel. -it's the illegal characters in the data, which still works fine with Sylvan,
+            //but columns that have "." in the column header won't be displayed in the DataGrid.
+            //The only issue I found in Sylvan.Data.Excel is when you import data where the first row is empty. Which isn't an issue for me.
+            DataTable dt = FixHeaders(SylvanReadExcelFile(fileDialog.FileName));
+
+            watch.Stop();
+            System.Diagnostics.Debug.WriteLine(watch.ElapsedMilliseconds + "\tms");
+
+            MainDataGrid.ItemsSource = dt.DefaultView;
+        }
+    }
+}
